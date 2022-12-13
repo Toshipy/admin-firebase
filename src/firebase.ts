3a9ee822085f409c8b7275e842e62c90b73b32bb
Toshipy <juantoshi1209@gmail.com>
2022-12-13 10:40:22 +0900

    google-sign-in added

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,9 +3,9 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  autoDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -14,8 +14,8 @@ const firebaseConfig = {
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const db =firebaseApp.firestore();
-export const auth =firebaseApp.auth();
-export const storage =firebaseApp.storage();
+export const db = firebaseApp.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
 export const provider = new firebase.auth.GoogleAuthProvider();
 
